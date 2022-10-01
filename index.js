adc563ac029f78e6391f85ff23b424d2cb211e1e
CailanGrott <106202528+CailanGrott@users.noreply.github.com>
2022-10-01 12:25:12 -0300

    12 files added, 3 deleted

--- index.js
+++
@@ -1,9 +0,0 @@
-let valorCompra = 100;
-let temConvenio = true;
-let temCartaoLoja = true;
-
-function quinze (valorCompra, temConvenio, temCartaoLoja) {
-    valorCompra - valorCompra * 0.15;
-}
-
-console.log(quinze(valorCompra, temConvenio, temCartaoLoja));
