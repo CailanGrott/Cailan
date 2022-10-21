6cb69b5b6c4ba41594f301208ebc0fe935cb645c
CailanGrott <106202528+CailanGrott@users.noreply.github.com>
2022-10-21 20:15:49 -0300

    1 file added

---
+++ Aula 04/0.4.2.js
@@ -0,0 +1,32 @@
+const alunos = [
+  {
+    nome: 'Mathias',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 7 },
+    ],
+  },
+  {
+    nome: 'Luiza',
+    notas: [
+      { cadeira: 'DW2', nota: 8 },
+      { cadeira: 'BD2', nota: 8 },
+    ],
+  },
+  {
+    nome: 'Pedro',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 10 },
+    ],
+  },
+];
+
+const somaTotal = alunos.reduce((acc, _nota) => {
+  const { nota } = _nota;
+  return acc + nota;
+}, 0);
+
+const mediaGeral = somaTotal / alunos.length;
+
+console.log(mediaGeral);
